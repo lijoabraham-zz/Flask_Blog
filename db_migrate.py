09d7dd9938a2ec31231baf333a686f9be2b01111
Lijo Abraham <lijoabraham1234@gmail.com>
2020-11-27 20:40:55 +0530

    DB migrations

---
+++ db_migrate.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flaskblog.config import Config
+
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+from flaskblog import create_app,db
+
+app = create_app()
+
+migrate = Migrate(app,db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
+
+
+
